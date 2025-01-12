5704feb65f3f8c7de60b9ce14d7fb977e1f7b35f
CoroSempi <corosempi@gmail.com>
2025-01-12 17:42:46 +0200

    Finshing The Project, Adding Last Touches :)

--- Scripts/trendy.js
+++ Scripts/trendy.js
@@ -68,8 +68,8 @@ function fetchProductsByCategory(category) {
   // If category is "all", fetch all products; otherwise, fetch products for a specific category
   var url =
     category === "all"
-      ? "https://dummyjson.com/products?sortBy=rate&order=asc&limit=60"
-      : `https://dummyjson.com/products/category/${category}?sortBy=rate&order=asc&limit=60`;
+      ? "https://dummyjson.com/products?sortBy=rating&order=asc&limit=60"
+      : `https://dummyjson.com/products/category/${category}?sortBy=rating&order=asc&limit=60`;
 
   xhr.open("GET", url);
 
